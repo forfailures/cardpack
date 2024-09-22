cd0604436b8d6d8339e0de96b2ce2b8300304485
folkengine <gaoler@electronicpanopticon.com>
2024-09-22 10:49:44 -0700

    maybe working...

--- src/cards/card/ranks/generic.rs
+++ src/cards/card/ranks/generic.rs
@@ -5,9 +5,10 @@ use crate::card_error::CardError;
 use crate::fluent::{FluentName, Named};
 
 pub trait Ranked: {
-    fn names(&self) -> Vec<&str>;
+    fn chars() -> Vec<char>;
+    fn names() -> Vec<&'static str>;
 
-    // fn is_valid_char(c: char) -> bool;
+    fn is_valid_char(c: &char) -> bool;
 }
 
 pub struct Rank<RankType> where RankType: Ranked {
@@ -17,9 +18,21 @@ pub struct Rank<RankType> where RankType: Ranked {
     phantom_data: PhantomData<RankType>
 }
 
-struct Standard52Rank{}
+impl<RankType: Ranked> Ranked for Rank<RankType> {
+    fn chars() -> Vec<char> {
+        RankType::chars()
+    }
 
-impl Rank<Standard52Rank> {
+    fn names() -> Vec<&'static str> {
+        RankType::names()
+    }
+
+    fn is_valid_char(c: &char) -> bool {
+        RankType::chars().contains(c)
+    }
+}
+
+impl<RankType> Rank<RankType> where RankType: Ranked {
     pub const ACE: &str = "ace";
     pub const KING: &str = "king";
     pub const QUEEN: &str = "queen";
@@ -33,103 +46,11 @@ impl Rank<Standard52Rank> {
     pub const FOUR: &str = "four";
     pub const THREE: &str = "three";
     pub const TWO: &str = "two";
-}
 
-impl<T: From<char> + Ranked> From<char> for GRank<T> {
-    fn from(c: char) -> GRank<T> {
-        match c {
-            '2' => GRank::new(crate::cards::card::ranks::standard52_rank::Standard52Rank::TWO),
-            '3' => GRank::new(crate::cards::card::ranks::standard52_rank::Standard52Rank::THREE),
-            '4' => GRank::new(crate::cards::card::ranks::standard52_rank::Standard52Rank::FOUR),
-            '5' => GRank::new(crate::cards::card::ranks::standard52_rank::Standard52Rank::FIVE),
-            '6' => GRank::new(crate::cards::card::ranks::standard52_rank::Standard52Rank::SIX),
-            '7' => GRank::new(crate::cards::card::ranks::standard52_rank::Standard52Rank::SEVEN),
-            '8' => GRank::new(crate::cards::card::ranks::standard52_rank::Standard52Rank::EIGHT),
-            '9' => GRank::new(crate::cards::card::ranks::standard52_rank::Standard52Rank::NINE),
-            'T' | 't' | '0' => GRank::new(crate::cards::card::ranks::standard52_rank::Standard52Rank::TEN),
-            'J' | 'j' => GRank::new(crate::cards::card::ranks::standard52_rank::Standard52Rank::JACK),
-            'Q' | 'q' => GRank::new(crate::cards::card::ranks::standard52_rank::Standard52Rank::QUEEN),
-            'K' | 'k' => GRank::new(crate::cards::card::ranks::standard52_rank::Standard52Rank::KING),
-            'A' | 'a' => GRank::new(crate::cards::card::ranks::standard52_rank::Standard52Rank::ACE),
-            _ => GRank::new(FluentName::BLANK),
-        }
-    }
-}
-
-// impl<T: Ranked> FromStr for GRank<T> {
-//     type Err = CardError;
-//
-//     fn from_str(s: &str) -> Result<Self, Self::Err> {
-//         if let Some(first_char) = s.chars().next() {
-//             let rank = GRank::<T>::from(first_char);
-//             if rank.name.is_blank() {
-//                 Err(CardError::InvalidFluentRank(s.to_string()))
-//             } else {
-//                 Ok(rank)
-//             }
-//         } else {
-//             Err(CardError::InvalidFluentRank(s.to_string()))
-//         }
-//     }
-// }
-
-impl Ranked for Standard52Rank {
-    fn names() -> Vec<&'static str> {
-        vec![
-            crate::cards::card::ranks::standard52_rank::Standard52Rank::ACE,
-            crate::cards::card::ranks::standard52_rank::Standard52Rank::KING,
-            crate::cards::card::ranks::standard52_rank::Standard52Rank::QUEEN,
-            crate::cards::card::ranks::standard52_rank::Standard52Rank::JACK,
-            crate::cards::card::ranks::standard52_rank::Standard52Rank::TEN,
-            crate::cards::card::ranks::standard52_rank::Standard52Rank::NINE,
-            crate::cards::card::ranks::standard52_rank::Standard52Rank::EIGHT,
-            crate::cards::card::ranks::standard52_rank::Standard52Rank::SEVEN,
-            crate::cards::card::ranks::standard52_rank::Standard52Rank::SIX,
-            crate::cards::card::ranks::standard52_rank::Standard52Rank::FIVE,
-            crate::cards::card::ranks::standard52_rank::Standard52Rank::FOUR,
-            crate::cards::card::ranks::standard52_rank::Standard52Rank::THREE,
-            crate::cards::card::ranks::standard52_rank::Standard52Rank::TWO,
-        ]
-    }
-
-    fn is_valid_char(c: char) -> bool {
-        match c {
-            '2'  |  '3'  |  '4'  |  '5'  |  '6'  |  '7'  |  '8'  |  '9'  |  'T'  |  't'  |  '0'  |  'J'  |  'j'  |  'Q'  |  'q'  |  'K'  |  'k'  |  'A'  |  'a' => true,
-            _ => false,
-        }
-    }
-}
-
-pub struct GRank<Rank> {
-    weight: u32,
-    prime: u32,
-    name: FluentName,
-    phantom_data: PhantomData<Rank>
-}
-
-
-impl<T: From<char> + Ranked> FromStr for GRank<T> {
-    type Err = CardError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if let Some(first_char) = s.chars().next() {
-            let rank = GRank::<T>::from(first_char);
-            if rank.value().is_blank() {
-                Err(CardError::InvalidFluentRank(s.to_string()))
-            } else {
-                Ok(rank)
-            }
-        } else {
-            Err(CardError::InvalidFluentRank(s.to_string()))
-        }
-    }
-}
-
-impl<T: Ranked + From<char>> GRank<T> {
-    fn new(name_str: &str) -> GRank<T> {
+    fn new(name_str: &str) -> Rank<RankType> {
         let name = FluentName::new(name_str);
 
-        GRank::<T> {
+        Rank::<RankType> {
             weight: name.weight(),
             prime: name.prime(),
             name,
@@ -137,13 +58,67 @@ impl<T: Ranked + From<char>> GRank<T> {
         }
     }
 
-    fn from_char(c: char) -> GRank<T> {
-        GRank::<T>::from(T::from(c))
-    }
+    // fn from_char(c: char) -> Rank<RankType> {
+    //     Rank::<RankType>::from(RankType::from(c))
+    // }
 
     #[must_use]
-    fn ranks() -> Vec<Self> {
-        T::names().iter().map(|name| Self::new(name)).collect()
+    fn ranks(&self) -> Vec<Self> {
+        RankType::names().iter().map(|name| Self::new(name)).collect()
+    }
+}
+
+struct Standard52Rank{}
+
+impl Ranked for Standard52Rank {
+    fn chars() -> Vec<char> {
+        vec!['2', '3', '4', '5', '6', '7', '8', '9', 'T', 't', '0', 'J', 'j', 'Q', 'q', 'K', 'k', 'A', 'a']
+    }
+
+    fn names() -> Vec<&'static str> {
+        vec![
+            Rank::<Standard52Rank>::ACE,
+            Rank::<Standard52Rank>::KING,
+            Rank::<Standard52Rank>::QUEEN,
+            Rank::<Standard52Rank>::JACK,
+            Rank::<Standard52Rank>::TEN,
+            Rank::<Standard52Rank>::NINE,
+            Rank::<Standard52Rank>::EIGHT,
+            Rank::<Standard52Rank>::SEVEN,
+            Rank::<Standard52Rank>::SIX,
+            Rank::<Standard52Rank>::FIVE,
+            Rank::<Standard52Rank>::FOUR,
+            Rank::<Standard52Rank>::THREE,
+            Rank::<Standard52Rank>::TWO,
+        ]
+    }
+
+    fn is_valid_char(c: &char) -> bool {
+        match c {
+            '2'  |  '3'  |  '4'  |  '5'  |  '6'  |  '7'  |  '8'  |  '9'  |  'T'  |  't'  |  '0'  |  'J'  |  'j'  |  'Q'  |  'q'  |  'K'  |  'k'  |  'A'  |  'a' => true,
+            _ => false,
+        }
+    }
+}
+
+impl<RankType: Ranked> From<char> for Rank<RankType> {
+    fn from(value: char) -> Self {
+        match value {
+            '2' => Rank::new(Rank::<RankType>::TWO),
+            '3' => Rank::new(Rank::<RankType>::THREE),
+            '4' => Rank::new(Rank::<RankType>::FOUR),
+            '5' => Rank::new(Rank::<RankType>::FIVE),
+            '6' => Rank::new(Rank::<RankType>::SIX),
+            '7' => Rank::new(Rank::<RankType>::SEVEN),
+            '8' => Rank::new(Rank::<RankType>::EIGHT),
+            '9' => Rank::new(Rank::<RankType>::NINE),
+            'T' | 't' | '0' => Rank::new(Rank::<RankType>::TEN),
+            'J' | 'j' => Rank::new(Rank::<RankType>::JACK),
+            'Q' | 'q' => Rank::new(Rank::<RankType>::QUEEN),
+            'K' | 'k' => Rank::new(Rank::<RankType>::KING),
+            'A' | 'a' => Rank::new(Rank::<RankType>::ACE),
+            _ => Rank::new(FluentName::BLANK),
+        }
     }
 }
 
@@ -155,9 +130,40 @@ mod rank__generic_tests {
 
     #[test]
     fn new() {
-        let rank = GRank::<Standard52Rank>::new(Standard52Rank::ACE);
-        assert_eq!(rank.name, FluentName::new(Standard52Rank::ACE));
+        let rank = Rank::<Standard52Rank>::new(Rank::<Standard52Rank>::ACE);
+
+        assert_eq!(rank.name, FluentName::new(Rank::<Standard52Rank>::ACE));
         assert_eq!(rank.weight, 12);
         assert_eq!(rank.prime, 41);
+    }
+
+    #[test]
+    fn is_valid_char() {
+        // let chars = Rank::chars();
+    }
+
+    #[test]
+    fn names() {
+        // let names = Rank::names();
+    }
+
+    #[test]
+    fn standard52_names() {
+        let names = Rank::<Standard52Rank>::names();
+
+        assert_eq!(names.len(), 13);
+        assert_eq!(names[0], Rank::<Standard52Rank>::ACE);
+        assert_eq!(names[1], Rank::<Standard52Rank>::KING);
+        assert_eq!(names[2], Rank::<Standard52Rank>::QUEEN);
+        assert_eq!(names[3], Rank::<Standard52Rank>::JACK);
+        assert_eq!(names[4], Rank::<Standard52Rank>::TEN);
+        assert_eq!(names[5], Rank::<Standard52Rank>::NINE);
+        assert_eq!(names[6], Rank::<Standard52Rank>::EIGHT);
+        assert_eq!(names[7], Rank::<Standard52Rank>::SEVEN);
+        assert_eq!(names[8], Rank::<Standard52Rank>::SIX);
+        assert_eq!(names[9], Rank::<Standard52Rank>::FIVE);
+        assert_eq!(names[10], Rank::<Standard52Rank>::FOUR);
+        assert_eq!(names[11], Rank::<Standard52Rank>::THREE);
+        assert_eq!(names[12], Rank::<Standard52Rank>::TWO);
     }
 }
