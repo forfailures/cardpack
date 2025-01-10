3b184b9f945f73b9653499fa7b5fcb7d6e657f72
ElectronicPanopticon <gaoler@electronicpanopticon.com>
2025-01-10 12:20:11 -0800

    Added decks file to refactored mod

---
+++ src/refact/decks.rs
@@ -0,0 +1,161 @@
+use crate::decks::FluentName;
+use crate::refact::{Rank, Ranked};
+use std::marker::PhantomData;
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct French {}
+
+impl French {
+    /// # REFACTOR WIN
+    pub const ACE: Rank<French> = Rank {
+        weight: 12,
+        index: French::ACE_INDEX,
+        phantom_data: PhantomData,
+    };
+    pub const KING: Rank<French> = Rank {
+        weight: 11,
+        index: French::KING_INDEX,
+        phantom_data: PhantomData,
+    };
+    pub const QUEEN: Rank<French> = Rank {
+        weight: 10,
+        index: French::QUEEN_INDEX,
+        phantom_data: PhantomData,
+    };
+    pub const JACK: Rank<French> = Rank {
+        weight: 9,
+        index: French::JACK_INDEX,
+        phantom_data: PhantomData,
+    };
+    pub const TEN: Rank<French> = Rank {
+        weight: 8,
+        index: French::TEN_INDEX,
+        phantom_data: PhantomData,
+    };
+    pub const NINE: Rank<French> = Rank {
+        weight: 7,
+        index: French::NINE_INDEX,
+        phantom_data: PhantomData,
+    };
+    pub const EIGHT: Rank<French> = Rank {
+        weight: 6,
+        index: French::EIGHT_INDEX,
+        phantom_data: PhantomData,
+    };
+    pub const SEVEN: Rank<French> = Rank {
+        weight: 5,
+        index: French::SEVEN_INDEX,
+        phantom_data: PhantomData,
+    };
+    pub const SIX: Rank<French> = Rank {
+        weight: 4,
+        index: French::SIX_INDEX,
+        phantom_data: PhantomData,
+    };
+    pub const FIVE: Rank<French> = Rank {
+        weight: 3,
+        index: French::FIVE_INDEX,
+        phantom_data: PhantomData,
+    };
+    pub const FOUR: Rank<French> = Rank {
+        weight: 2,
+        index: French::FOUR_INDEX,
+        phantom_data: PhantomData,
+    };
+    pub const TREY: Rank<French> = Rank {
+        weight: 1,
+        index: French::TREY_INDEX,
+        phantom_data: PhantomData,
+    };
+    pub const DEUCE: Rank<French> = Rank {
+        weight: 0,
+        index: French::DEUCE_INDEX,
+        phantom_data: PhantomData,
+    };
+
+    pub const ACE_INDEX: char = 'A';
+    pub const KING_INDEX: char = 'K';
+    pub const QUEEN_INDEX: char = 'Q';
+    pub const JACK_INDEX: char = 'J';
+    pub const TEN_INDEX: char = 'T';
+    pub const NINE_INDEX: char = '9';
+    pub const EIGHT_INDEX: char = '8';
+    pub const SEVEN_INDEX: char = '7';
+    pub const SIX_INDEX: char = '6';
+    pub const FIVE_INDEX: char = '5';
+    pub const FOUR_INDEX: char = '4';
+    pub const TREY_INDEX: char = '3';
+    pub const DEUCE_INDEX: char = '2';
+
+    pub const FLUENT_KEY_ACE: &'static str = "ace";
+    pub const FLUENT_KEY_KING: &'static str = "king";
+    pub const FLUENT_KEY_QUEEN: &'static str = "queen";
+    pub const FLUENT_KEY_JACK: &'static str = "jack";
+    pub const FLUENT_KEY_TEN: &'static str = "ten";
+    pub const FLUENT_KEY_NINE: &'static str = "nine";
+    pub const FLUENT_KEY_EIGHT: &'static str = "eight";
+    pub const FLUENT_KEY_SEVEN: &'static str = "seven";
+    pub const FLUENT_KEY_SIX: &'static str = "six";
+    pub const FLUENT_KEY_FIVE: &'static str = "five";
+    pub const FLUENT_KEY_FOUR: &'static str = "four";
+    pub const FLUENT_KEY_TREY: &'static str = "three";
+    pub const FLUENT_KEY_DEUCE: &'static str = "two";
+}
+
+impl<French: Ranked> Default for Rank<French> {
+    fn default() -> Self {
+        Rank {
+            weight: 0,
+            index: '_',
+            phantom_data: PhantomData,
+        }
+    }
+}
+
+/// # REFACTOR WIN
+impl From<char> for Rank<French> {
+    fn from(c: char) -> Self {
+        match c {
+            French::ACE_INDEX | 'a' => French::ACE,
+            French::KING_INDEX | 'k' => French::KING,
+            French::QUEEN_INDEX | 'q' => French::QUEEN,
+            French::JACK_INDEX | 'j' => French::JACK,
+            French::TEN_INDEX | 't' | '0' => French::TEN,
+            French::NINE_INDEX => French::NINE,
+            French::EIGHT_INDEX => French::EIGHT,
+            French::SEVEN_INDEX => French::SEVEN,
+            French::SIX_INDEX => French::SIX,
+            French::FIVE_INDEX => French::FIVE,
+            French::FOUR_INDEX => French::FOUR,
+            French::TREY_INDEX => French::TREY,
+            French::DEUCE_INDEX => French::DEUCE,
+            _ => Rank::<French>::default(),
+        }
+    }
+}
+
+impl Ranked for French {
+    /// ```
+    /// use cardpack::refactored::*;
+    ///
+    /// assert_eq!(French::ACE.get_name().fluent_name_string(), French::FLUENT_KEY_ACE);
+    /// ```
+    fn name(index: char) -> FluentName {
+        match index {
+            French::ACE_INDEX => FluentName::new(French::FLUENT_KEY_ACE),
+            French::KING_INDEX => FluentName::new(French::FLUENT_KEY_KING),
+            French::QUEEN_INDEX => FluentName::new(French::FLUENT_KEY_QUEEN),
+            French::JACK_INDEX => FluentName::new(French::FLUENT_KEY_JACK),
+            French::TEN_INDEX => FluentName::new(French::FLUENT_KEY_TEN),
+            French::NINE_INDEX => FluentName::new(French::FLUENT_KEY_NINE),
+            French::EIGHT_INDEX => FluentName::new(French::FLUENT_KEY_EIGHT),
+            French::SEVEN_INDEX => FluentName::new(French::FLUENT_KEY_SEVEN),
+            French::SIX_INDEX => FluentName::new(French::FLUENT_KEY_SIX),
+            French::FIVE_INDEX => FluentName::new(French::FLUENT_KEY_FIVE),
+            French::FOUR_INDEX => FluentName::new(French::FLUENT_KEY_FOUR),
+            French::TREY_INDEX => FluentName::new(French::FLUENT_KEY_TREY),
+            French::DEUCE_INDEX => FluentName::new(French::FLUENT_KEY_DEUCE),
+            _ => FluentName::new(FluentName::BLANK),
+        }
+    }
+}
--- src/refact/mod.rs
+++ src/refact/mod.rs
@@ -1,4 +1,9 @@
-use crate::decks::FluentName;
+pub mod decks;
+pub use crate::refact::decks::French;
+
+pub use crate::decks::FluentName;
+use crate::types::utils::Bit;
+
 use std::fmt::{Display, Formatter};
 use std::marker::PhantomData;
 
@@ -31,6 +36,12 @@ where
         RankType::name(self.index)
     }
 
+    /// Returns the xth prime number where x is the weight of the rank.
+    ///
+    ///The goal of this function is to replace the earlier version of the struct that stored the
+    /// prime number as a field. In refactoring this code, I am trying to take a minimalist approach
+    /// to the types. The original structs come from when I was very new to Rust.
+    ///
     /// ```
     /// use cardpack::refactored::*;
     ///
@@ -80,7 +91,40 @@ where
 
     //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
     // CKC Calculations
+    /// Used to generate the `Rank` portion of the `Card`'s binary number,
+    /// aka [Cactus Kev](https://suffe.cool/poker/evaluator.html) number.
+    ///
+    /// ```
+    /// use cardpack::refactored::*;
+    ///
+    /// assert_eq!(0b00000000_00000001_00000000_00000010, French::DEUCE.ckc_number());
+    /// assert_eq!(0b00000000_00000010_00000001_00000011, French::TREY.ckc_number());
+    /// assert_eq!(0b00000000_00000100_00000010_00000101, French::FOUR.ckc_number());
+    /// assert_eq!(0b00000000_00001000_00000011_00000111, French::FIVE.ckc_number());
+    /// assert_eq!(0b00000000_00010000_00000100_00001011, French::SIX.ckc_number());
+    /// assert_eq!(0b00000000_00100000_00000101_00001101, French::SEVEN.ckc_number());
+    /// assert_eq!(0b00000000_01000000_00000110_00010001, French::EIGHT.ckc_number());
+    /// assert_eq!(0b00000000_10000000_00000111_00010011, French::NINE.ckc_number());
+    /// assert_eq!(0b00000001_00000000_00001000_00010111, French::TEN.ckc_number());
+    /// assert_eq!(0b00000010_00000000_00001001_00011101, French::JACK.ckc_number());
+    /// assert_eq!(0b00000100_00000000_00001010_00011111, French::QUEEN.ckc_number());
+    /// assert_eq!(0b00001000_00000000_00001011_00100101, French::KING.ckc_number());
+    /// assert_eq!(0b00010000_00000000_00001100_00101001, French::ACE.ckc_number());
+    /// ```
+    #[must_use]
+    pub fn ckc_number(&self) -> u32 {
+        self.get_bits() | self.get_shift8() | self.get_prime()
+    }
 
+    #[must_use]
+    fn get_bits(&self) -> u32 {
+        1 << (Bit::RANK_FLAG_SHIFT + self.weight)
+    }
+
+    #[must_use]
+    pub fn get_shift8(&self) -> u32 {
+        self.weight << 8
+    }
 }
 
 impl<RankType> Display for Rank<RankType>
@@ -97,127 +141,32 @@ where
     }
 }
 
-#[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub struct French {}
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod ranks {
+    use super::*;
 
-impl French {
-    pub const ACE: Rank<French> = Rank {
-        weight: 12,
-        index: French::ACE_INDEX,
-        phantom_data: PhantomData,
-    };
-    pub const KING: Rank<French> = Rank {
-        weight: 11,
-        index: French::KING_INDEX,
-        phantom_data: PhantomData,
-    };
-    pub const QUEEN: Rank<French> = Rank {
-        weight: 10,
-        index: French::QUEEN_INDEX,
-        phantom_data: PhantomData,
-    };
-    pub const JACK: Rank<French> = Rank {
-        weight: 9,
-        index: French::JACK_INDEX,
-        phantom_data: PhantomData,
-    };
-    pub const TEN: Rank<French> = Rank {
-        weight: 8,
-        index: French::TEN_INDEX,
-        phantom_data: PhantomData,
-    };
-    pub const NINE: Rank<French> = Rank {
-        weight: 7,
-        index: French::NINE_INDEX,
-        phantom_data: PhantomData,
-    };
-    pub const EIGHT: Rank<French> = Rank {
-        weight: 6,
-        index: French::EIGHT_INDEX,
-        phantom_data: PhantomData,
-    };
-    pub const SEVEN: Rank<French> = Rank {
-        weight: 5,
-        index: French::SEVEN_INDEX,
-        phantom_data: PhantomData,
-    };
-    pub const SIX: Rank<French> = Rank {
-        weight: 4,
-        index: French::SIX_INDEX,
-        phantom_data: PhantomData,
-    };
-    pub const FIVE: Rank<French> = Rank {
-        weight: 3,
-        index: French::FIVE_INDEX,
-        phantom_data: PhantomData,
-    };
-    pub const FOUR: Rank<French> = Rank {
-        weight: 2,
-        index: French::FOUR_INDEX,
-        phantom_data: PhantomData,
-    };
-    pub const TREY: Rank<French> = Rank {
-        weight: 1,
-        index: French::TREY_INDEX,
-        phantom_data: PhantomData,
-    };
-    pub const DEUCE: Rank<French> = Rank {
-        weight: 0,
-        index: French::DEUCE_INDEX,
-        phantom_data: PhantomData,
-    };
+    #[test]
+    fn get_prime() {
+        assert_eq!(French::DEUCE.get_prime(), 2);
+        assert_eq!(French::TREY.get_prime(), 3);
+        assert_eq!(French::FOUR.get_prime(), 5);
+        assert_eq!(French::FIVE.get_prime(), 7);
+        assert_eq!(French::SIX.get_prime(), 11);
+        assert_eq!(French::SEVEN.get_prime(), 13);
+        assert_eq!(French::EIGHT.get_prime(), 17);
+        assert_eq!(French::NINE.get_prime(), 19);
+        assert_eq!(French::TEN.get_prime(), 23);
+        assert_eq!(French::JACK.get_prime(), 29);
+        assert_eq!(French::QUEEN.get_prime(), 31);
+        assert_eq!(French::KING.get_prime(), 37);
+        assert_eq!(French::ACE.get_prime(), 41);
+    }
 
-    pub const ACE_INDEX: char = 'A';
-    pub const KING_INDEX: char = 'K';
-    pub const QUEEN_INDEX: char = 'Q';
-    pub const JACK_INDEX: char = 'J';
-    pub const TEN_INDEX: char = 'T';
-    pub const NINE_INDEX: char = '9';
-    pub const EIGHT_INDEX: char = '8';
-    pub const SEVEN_INDEX: char = '7';
-    pub const SIX_INDEX: char = '6';
-    pub const FIVE_INDEX: char = '5';
-    pub const FOUR_INDEX: char = '4';
-    pub const TREY_INDEX: char = '3';
-    pub const DEUCE_INDEX: char = '2';
+    #[test]
+    fn update_weight() {
+        let heavy_card = French::TREY.update_weight(21);
 
-    pub const FLUENT_KEY_ACE: &'static str = "ace";
-    pub const FLUENT_KEY_KING: &'static str = "king";
-    pub const FLUENT_KEY_QUEEN: &'static str = "queen";
-    pub const FLUENT_KEY_JACK: &'static str = "jack";
-    pub const FLUENT_KEY_TEN: &'static str = "ten";
-    pub const FLUENT_KEY_NINE: &'static str = "nine";
-    pub const FLUENT_KEY_EIGHT: &'static str = "eight";
-    pub const FLUENT_KEY_SEVEN: &'static str = "seven";
-    pub const FLUENT_KEY_SIX: &'static str = "six";
-    pub const FLUENT_KEY_FIVE: &'static str = "five";
-    pub const FLUENT_KEY_FOUR: &'static str = "four";
-    pub const FLUENT_KEY_TREY: &'static str = "three";
-    pub const FLUENT_KEY_DEUCE: &'static str = "two";
-}
-
-impl Ranked for French {
-    /// ```
-    /// use cardpack::refactored::*;
-    ///
-    /// assert_eq!(French::ACE.get_name().fluent_name_string(), French::FLUENT_KEY_ACE);
-    /// ```
-    fn name(index: char) -> FluentName {
-        match index {
-            French::ACE_INDEX => FluentName::new(French::FLUENT_KEY_ACE),
-            French::KING_INDEX => FluentName::new(French::FLUENT_KEY_KING),
-            French::QUEEN_INDEX => FluentName::new(French::FLUENT_KEY_QUEEN),
-            French::JACK_INDEX => FluentName::new(French::FLUENT_KEY_JACK),
-            French::TEN_INDEX => FluentName::new(French::FLUENT_KEY_TEN),
-            French::NINE_INDEX => FluentName::new(French::FLUENT_KEY_NINE),
-            French::EIGHT_INDEX => FluentName::new(French::FLUENT_KEY_EIGHT),
-            French::SEVEN_INDEX => FluentName::new(French::FLUENT_KEY_SEVEN),
-            French::SIX_INDEX => FluentName::new(French::FLUENT_KEY_SIX),
-            French::FIVE_INDEX => FluentName::new(French::FLUENT_KEY_FIVE),
-            French::FOUR_INDEX => FluentName::new(French::FLUENT_KEY_FOUR),
-            French::TREY_INDEX => FluentName::new(French::FLUENT_KEY_TREY),
-            French::DEUCE_INDEX => FluentName::new(French::FLUENT_KEY_DEUCE),
-            _ => FluentName::new(FluentName::BLANK),
-        }
+        assert_eq!(heavy_card.get_prime(), 0);
     }
 }
